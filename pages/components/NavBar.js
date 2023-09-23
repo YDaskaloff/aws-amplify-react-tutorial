7ae240df576d8c6135f31795c74bc3b9397f1cc0
ydaskaloff <ydaskaloff@outlook.com>
2023-09-23 19:36:10 +0300

    added navbar

---
+++ pages/components/NavBar.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import React, {useState, useEffect} from 'react'
+import '../../configureAmplify'
+
+const NavBar = () => {
+    const [signedUser, setSignedUser] = useState(false);
+
+    return (
+        <nav className="flex justify-center pt-3 pb-3 space-x-4 border-b bg-cyan-500 border-gray-300">
+            {
+                [
+                    ["Home", "/"],
+                    ["Create Post", '/create-post'],
+                    ["Profile", '/profile'],
+                ].map(([title, url]) => (
+                    <Link legacyBehavior href={url} key={title}>
+                        <a className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate hover:text-slate-900">
+                            {title}
+                        </a>
+                    </Link>
+                ))
+            }
+        </nav>
+    )
+}
+
+export default NavBar;
