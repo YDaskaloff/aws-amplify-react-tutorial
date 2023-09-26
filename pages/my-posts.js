5828e6fc553879e9e07dfe8df0a46516a0e7f755
ydaskaloff <ydaskaloff@outlook.com>
2023-09-26 09:51:05 +0300

    styled my posts

--- pages/my-posts.js
+++ pages/my-posts.js
@@ -1,54 +1,101 @@
-import { API, Auth } from 'aws-amplify';
+import { Auth, API } from 'aws-amplify';
+import Moment from 'moment';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
+import { deletePost as deletePostMutation } from '../src/graphql/mutations';
 import { postsByUsername } from '../src/graphql/queries';
 
-const MyPosts = () => {
+export default function MyPosts() {
     const [posts, setPosts] = useState([]);
-
-    const fetchPosts = async () => {
-        const user = await Auth.currentAuthenticatedUser();
-        const { username, attributes: { sub: userId } } = user;
-
-        const postData = await API.graphql({
-            query: postsByUsername,
-            variables: {
-                username: `${userId}::${username}`
-            },
-        });
-
-        setPosts(postData.data.postsByUsername.items);
-    };
-
     useEffect(() => {
         fetchPosts();
     }, []);
+    async function fetchPosts() {
+        const { username, attributes: { sub: userId } } = await Auth.currentAuthenticatedUser();
+        const postData = await API.graphql({
+            query: postsByUsername,
+            variables: { username: `${userId}::${username}` },
+        });
+
+        // to get image thumbnails
+        const { items } = postData.data.postsByUsername;
+
+        // Fetch images from S3 for posts that contain a cover image
+        // const postsWithImages = await Promise.all(
+        //     items.map(async (post) => {
+        //         if (post.coverImage) {
+        //             post.coverImage = await Storage.get(post.coverImage);
+        //         }
+        //         return post;
+        //     })
+        // );
+        setPosts(items);
+    // setPosts(postData.data.postsByUsername.items);
+    }
+    const deletePost = async (id) => {
+        await API.graphql({
+            query: deletePostMutation,
+            variables: { input: { id } },
+            authMode: 'AMAZON_COGNITO_USER_POOLS',
+        });
+        fetchPosts();
+    };
 
     return (
         <div>
-            <h1 className="text-sky-400 text-3xl font-semibold tracking-wide mt-6 mb-2">
-                My posts
-            </h1>
-
-            { posts.map(
-                (post) => (
-                    <Link key={post.id} href={`/posts/${post.id}`}>
-                        <div className="cursor-pointer border-b border-gray-300 mt-8 pb-4">
-                            <h2 className="text-xl font-semibold">
-                                {post.title}
-                            </h2>
-                            <p className="text-gray-500 mt-2" >
-                                Author:
+            {posts.map((post, index) => (
+                <div
+                  key={index}
+                  className="py-8 px-8 max-w-xxl mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-1 sm:flex
+          sm:items-center sm:space-y-0 sm:space-x-6 mb-2"
+                >
+                    {/* {post.coverImage && (
+                        <img
+                          className="w-36 h-36 bg-contain bg-center rounded-full sm:mx-0 sm:shrink-0"
+                          src={post.coverImage}
+                        />
+                    )} */}
+                    <div className="text-center space-y-2 sm:text-left">
+                        <div className="space-y-0.5">
+                            <p className="text-lg text-black font-semibold">{post.title}</p>
+                            <p className="text-slate-500 font-medium">
+                                Created on:
                                 {' '}
-                                {post.username}
+                                {Moment(post.createdAt).format('ddd, MMM hh:mm a')}
                             </p>
                         </div>
-                    </Link>
-                )
-            )}
+                        <div
+                          className="sm:py-4 sm:flex
+        sm:items-center sm:space-y-0 sm:space-x-1"
+                        >
+                            <p
+                              className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200
+    hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none
+    focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+                            >
+                                <Link href={`/edit-post/${post.id}`}>Edit Post</Link>
+                            </p>
+
+                            <p
+                              className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200
+    hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none
+    focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+                            >
+                                <Link href={`/posts/${post.id}`}>View Post</Link>
+                            </p>
+
+                            <button
+                              type="button"
+                              className="text-sm mr-4 text-red-500"
+                              onClick={() => deletePost(post.id)}
+                            >
+                                Delete Post
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
     );
-};
-
-export default MyPosts;
+}
